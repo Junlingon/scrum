4347891e16ecc96c4e0d7c04784e155cedb3e606
JunlinGong <913756523@qq.com>
2023-03-12 14:03:39 +0800

    feat: 项目列表搜索

--- src/pages/components/project_search.js
+++ src/pages/components/project_search.js
@@ -1,11 +1,51 @@
-import { Input, Form, Button } from 'antd'
+import { Input, Select, Form, Button } from 'antd'
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { getProjectListAsync, select_orgs, select_users, set_current_page, set_search_query } from '../../redux/slice/project';
+
 
 function ProjectSearch() {
 
+    // console.log('项目搜索框 render')
+
+    const dispatch = useDispatch()
+
+    const orgs = useSelector(select_orgs)
+    const users = useSelector(select_users)
+
     const [form] = Form.useForm();
 
+
+    async function search_click() {
+        const form_data = await form.validateFields()
+        if (form_data) {
+            dispatch(set_search_query(form_data))
+            dispatch(set_current_page(1))
+            dispatch(getProjectListAsync())
+        }
+    }
+
+    function reset() {
+        form.resetFields()
+    }
+
+    const orgs_options = orgs.map((item) => {
+        return {
+            value: item.name,
+            label: item.name
+        }
+    })
+
+    const users_options = users.map((item) => {
+        return {
+            value: item.username,
+            label: item.username
+        }
+    })
+
     return (
-        <Form layout="inline" name='ProjectSearch' form={form} >
+        <Form layout="inline" form={form} >
             <Form.Item
                 name="name"
                 style={{ width: 180 }}
@@ -18,23 +58,23 @@ function ProjectSearch() {
 
                 style={{ width: 180 }}
             >
-                {/* <Select
-                    options={['研发部门']}
+                <Select
+                    options={orgs_options}
                 >
-                </Select> */}
+                </Select>
             </Form.Item>
             <Form.Item
                 label="负责人"
                 name="owner"
                 style={{ width: 180 }}
             >
-                {/* <Select
-                    options={['李明明']}
+                <Select
+                    options={users_options}
                 >
-                </Select> */}
+                </Select>
             </Form.Item>
-            <Button type="">重置</Button>
-            <Button type="primary">查询</Button>
+            <Button onClick={reset} type="">重置</Button>
+            <Button onClick={search_click} type="primary">查询</Button>
         </Form>
     )
 }
